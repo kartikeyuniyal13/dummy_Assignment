2ff8b97532f4f97c5f7bf98665e831ee83fade4a
Kartikey Uniyal <kartikeyuniyal13@gmail.com>
2024-08-16 21:05:06 +0530

    user action completed

--- src/lib/actions/user.action.js
+++ src/lib/actions/user.action.js
@@ -1,14 +1,23 @@
-import { connectToDb } from "../db";
+import { User } from '@/dbModel/User';
+import { connectToDb } from '../db'; // Adjust the path as per your project structure
+
+async function createUser(userData) {
+  try {
+    await connectToDb();
+    const newUser = new User(userData);
+    const savedUser = await newUser.save();
+    return savedUser;
+  } catch (error) {
+    throw new Error(`Error creating user: ${error.message}`);
+  }
+}
 
 
-export async function createUser (){
-    try{
-        connectToDb();
-
-        const newUser=
-    }
-    catch(error){
-        console.error("Error creating user", error);
-    }
-
+async function deleteUser(clerkId) {
+  try {
+    const deletedUser = await User.findOneAndDelete({ clerkId });
+    return deletedUser;
+  } catch (error) {
+    throw new Error(`Error deleting user by clerkId: ${error.message}`);
+  }
 }
