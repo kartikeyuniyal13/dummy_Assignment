f414ea881090374dff6cee63fa51f3eb75570e33
Kartikey Uniyal <kartikeyuniyal13@gmail.com>
2024-08-16 20:40:43 +0530

    updated dependencies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      remotePatterns: ['m.media-amazon.com'],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  reactStrictMode: true,
+  images: {
+    domains: ['m.media-amazon.com'], 
+  },
+};
+
+module.exports = nextConfig;
