e674b576b5f2947b166af9f9eb6008f936db0ce6
Kartikey Uniyal <kartikeyuniyal13@gmail.com>
2024-08-17 08:50:09 +0530

    check-6

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -1,6 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import {createUser} from '@/lib/actions/user.action'
+import { NextResponse } from 'next/server'
 
 export async function POST(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
