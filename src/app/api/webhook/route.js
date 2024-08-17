78081ed7ee597b9b33647c3036bfecee911091eb
Kartikey Uniyal <kartikeyuniyal13@gmail.com>
2024-08-17 07:52:08 +0530

    checking 4

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -1,89 +1,57 @@
-/* eslint-disable camelcase */
-import { Webhook } from "svix";
-import { headers } from "next/headers";
-
-import { createUser, deleteUser} from "@/lib/actions/user.action";
-import { createCart } from "@/lib/actions/cart.action";
-import { NextResponse } from "next/server";
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
+import { WebhookEvent } from '@clerk/nextjs/server'
 
 export async function POST(req) {
-  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET
 
   if (!WEBHOOK_SECRET) {
-    throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
-    );
+    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
 
-  const headerPayload = headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
+  // Get the headers
+  const headerPayload = headers()
+  const svix_id = headerPayload.get('svix-id')
+  const svix_timestamp = headerPayload.get('svix-timestamp')
+  const svix_signature = headerPayload.get('svix-signature')
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
+    return new Response('Error occured -- no svix headers', {
       status: 400,
-    });
+    })
   }
 
   // Get the body
-  const payload = await req.json();
-  const body = JSON.stringify(payload);
+  const payload = await req.json()
+  const body = JSON.stringify(payload)
 
-  // Create a new SVIX instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
+  // Create a new Svix instance with your secret.
+  const wh = new Webhook(WEBHOOK_SECRET)
 
   let evt;
 
   // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature,
-    }) ;
+      'svix-id': svix_id,
+      'svix-timestamp': svix_timestamp,
+      'svix-signature': svix_signature,
+    }) 
   } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
+    console.error('Error verifying webhook:', err)
+    return new Response('Error occured', {
       status: 400,
-    });
+    })
   }
 
-  const eventType = evt.type;
-  console.log(eventType);
+  // Do something with the payload
+  // For this guide, you simply log the payload to the console
+  const { id } = evt.data
+  const eventType = evt.type
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
+  console.log('Webhook body:', body)
 
-  //   Create User webhook
-  if (eventType === "user.created") {
-    // Create a user in the database
-    const { id, email_addresses, image_url, username, first_name, last_name } =
-      evt.data;
-    const mongoCart = await createCart({
-      userId: id,
-      items: [],
-    });
-    const mongoUser = await createUser({
-      clerkId: id,
-      name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
-      username: username,
-      email: email_addresses[0].email_address,
-      cartId:"jdskf"
-    });
-    return NextResponse.json({ message: "OK", user: mongoUser });
-  }
-
-  //   Update User webhook
-
-
-  //   Delete User webhook
-  if (eventType === "user.deleted") {
-    const { id } = evt.data;
-    // Delete user from the database
-    const deletedUser = await deleteUser({
-      clerkId: id, // ----> id sometime may be null
-    });
-    return NextResponse.json({ message: "OK", user: deletedUser });
-  }
-
-  return new Response("", { status: 201 });
+  return new Response('', { status: 200 })
 }
